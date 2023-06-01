82ec20e831002e96b61217f95293bf89b695fa51
Lyossauce <julien.mercier-chauve@hotmail.fr>
2023-06-01 18:10:34 +0200

    fix second player init

--- src/apps/games/services/createGame.ts
+++ src/apps/games/services/createGame.ts
@@ -24,25 +24,33 @@ export const createGame = async (input: CreateGameInput): Promise<string> => {
     id: randomUUID(),
     _gameId: game.id,
     name: input.player1Name,
-    ...NEW_PLAYER,
+    healthPoints: 30,
+    mana: 0,
+    handCards: [],
+    hiddenCards: [...(NEW_PLAYER.hiddenCards as string[])],
+    turnNumber: 0,
     playOrder: 1,
+    isPlaying: true,
   } as PlayerDbRecord;
 
   const player2 : PlayerDbRecord = {
     id: randomUUID(),
     _gameId: game.id,
     name: input.player2Name,
-    ...NEW_PLAYER,
+    healthPoints: 30,
+    mana: 0,
+    handCards: [],
+    hiddenCards: [...(NEW_PLAYER.hiddenCards as string[])],
+    turnNumber: 0,
     playOrder: 2,
+    isPlaying: false,
   } as PlayerDbRecord;
-
 
   addCardToPlayerHand(player1);
 
   const players = [player1, player2];
 
   await GameRepository.createOne(game);
-
   await PlayerRepository.createMany(players);
 
   return game.id;
